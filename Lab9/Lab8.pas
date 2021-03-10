f606f0c05772a28d2dc790284caa1bd4c1643c0c
Mailo_Fenn <nikitakulkov7@gmail.com>
2021-03-10 20:51:23 +0300

    add lab9

---
+++ Lab9/Lab8.pas
@@ -0,0 +1,12 @@
+﻿uses Lib;
+ 
+var
+   i,j,sum:integer;
+   a: arr;
+ 
+begin
+   for i:= 1 to 10 do
+      for j := 1 to 10 do
+      a[i][j] := i + j;
+   write(func(a));
+end.
---
+++ Lab9/Lib.pas
@@ -0,0 +1,15 @@
+﻿Unit Lib;
+Interface
+  type arr = array[1..10, 1..10] of integer;
+  function func(var a: arr): integer;
+Implementation
+  function func(var a: arr): integer;
+  var i,j,count: integer;
+  begin
+    for i:= 1 to 10 do
+      for j := 1 to 10 do
+        if i = j then
+          count += a[i][j];
+    Result := count;
+  end;
+ end.
